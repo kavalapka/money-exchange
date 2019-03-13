db07f84199c0439e3dd6efde946c4eafb078eaf2
Katerina Zenchik <e.zenchik@gmail.com>
2019-03-13 21:45:51 +0300

    add currency dividing function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,24 @@
 // PLEASE DON'T change function name
 module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+
+  var change = {"H":50,"Q":25,"D":10,"N":5,"P":1};
+  var ret = {};
+  var error  = {error: "You are rich, my friend! We don\'t have so much coins for exchange"};
+
+  if (currency > 10000) {
+    return error
+  }
+
+  if (currency <= 0) {return ret}
+
+  for (var key in change) {
+    var coin = currency / change[key] ^ 0;
+    if (coin === 0) {
+      continue
+    }
+    ret[key] = coin;
+    currency -= change[key]*coin;
+
+  }
+  return ret;
 }
